fa93cbca43b060ba8e272f17cb0db55727f49989
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2025-12-19 13:05:48 +0530

    multer

--- client/app/components/FileUpload.tsx
+++ client/app/components/FileUpload.tsx
@@ -1,17 +1,26 @@
 "use client";
-import { Upload } from "lucide-react";
+import { Form, Upload } from "lucide-react";
 export default function FileUpload() {
   const handleFileUploadClick = () => {
     const el = document.createElement("input");
     el.setAttribute("type", "file");
     el.setAttribute("accept", "application/pdf");
-    el.click();
-    el.addEventListener("change", (ev) => {
+
+    el.addEventListener("change", async (ev) => {
       if (el.files && el.files.length > 0) {
         const file = el.files.item(0);
-        console.log("Selected file:", file);
+        const formData = new FormData();
+        if (file) {
+          formData.append("pdf", file);
+          await fetch("http://localhost:8000/upload/pdf", {
+            method: "POST",
+            body: formData,
+          });
+          console.log("File uploaded successfully");
+        }
       }
     });
+    el.click();
   };
   return (
     <div className="flex text-white justify-center items-center bg-gray-500 rounded-lg p-3 shadow-2xl border-white border-2">
--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
-const express = require("express");
+import express from "express";
+
+import multer from "multer";
+import cors from "cors";
 const app = express();
 
-const cors = require("cors");
-
 app.use(cors());
-
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + "- " + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+  res.send({ message: "File uploaded successfully", file: req.file });
 });
 
 const PORT = process.env.PORT || 8000;
