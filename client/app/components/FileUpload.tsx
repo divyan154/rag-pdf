592af63d83a7b2939a3342809d30c00e1645e025
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2025-12-19 12:04:24 +0530

    file input

--- client/app/components/FileUpload.tsx
+++ client/app/components/FileUpload.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+"use client";
 import { Upload } from "lucide-react";
 export default function FileUpload() {
+  const handleFileUploadClick = () => {
+    const el = document.createElement("input");
+    el.setAttribute("type", "file");
+    el.setAttribute("accept", "application/pdf");
+    el.click();
+    el.addEventListener("change", (ev) => {
+      if (el.files && el.files.length > 0) {
+        const file = el.files.item(0);
+        console.log("Selected file:", file);
+      }
+    });
+  };
   return (
-    <div className="flex text-white justify-center items-center bg-gray-500 rounded-lg p-10 shadow-2xl border-white border-2">
-      <div className="flex justify-center items-center flex-col gap-6">
+    <div className="flex text-white justify-center items-center bg-gray-500 rounded-lg p-3 shadow-2xl border-white border-2">
+      <div
+        onClick={handleFileUploadClick}
+        className="flex justify-center items-center flex-col"
+      >
         <h3 className="">Upload Your Pdf</h3>
-        <Upload className="w-10 h-10" />
+        <Upload className="w-3 h-3" />
       </div>
     </div>
   );
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div className="min-h-screen w-screen flex">
-        <div className="min-h-screen w-[30vw] flex justify-center items-center p-4">
+        <div className="min-h-screen w-[30vw] flex justify-center items-center p-5">
           <FileUpload />
         </div>
         <div className="min-h-screen w-[70vw] border-l-4 border-white">2</div>
