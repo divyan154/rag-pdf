fa24903cfc0a4fd89b28f28f3a56a6df2dbda46f
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2025-12-20 18:38:14 +0530

    Vector Store

--- server/worker.js
+++ server/worker.js
@@ -1,5 +1,7 @@
 import { Worker } from "bullmq";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { QdrantVectorStore } from "@langchain/qdrant";
 
 const worker = new Worker(
   "ProcessPdfQueue",
@@ -20,6 +22,22 @@ Store embeddings in vector database
     // console.log("Loading PDF from path....");
     const docs = await loader.load();
     // console.log("Loaded documents:", docs);
+
+    //Generate Embeddings
+    const embeddings = new OllamaEmbeddings({
+      model: "nomic-embed-text",
+      baseUrl: "http://localhost:11434",
+    });
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: "http://localhost:6333",
+        collectionName: "langchainjs-testing",
+      }
+    );
+    await vectorStore.addDocuments(docs);
+    console.log("Added documents to Qdrant vector store");
   },
+
   { concurrency: 100, connection: { host: "localhost", port: 6379 } }
 );
