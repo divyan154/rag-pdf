793fdce209abb9f39c99b30c785aaab70169d39d
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2025-12-20 17:42:11 +0530

    Console logs

--- server/worker.js
+++ server/worker.js
@@ -17,9 +17,9 @@ Store embeddings in vector database
     //load PDF
     // console.log("pdf path:", data);
     const loader = new PDFLoader(data.path);
-    console.log("Loading PDF from path....");
+    // console.log("Loading PDF from path....");
     const docs = await loader.load();
-    console.log("Loaded documents:", docs);
+    // console.log("Loaded documents:", docs);
   },
   { concurrency: 100, connection: { host: "localhost", port: 6379 } }
 );
